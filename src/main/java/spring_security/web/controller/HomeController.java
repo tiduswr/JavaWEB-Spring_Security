286b11c9cb8819bb042b43982b0c1efeaa8618e1
Harllem Nascimento <harllemnascimento@gmail.com>
2022-08-23 11:46:43 -0300

    Seção 2: Iniciando com Spring Security

--- src/main/java/spring_security/web/controller/HomeController.java
+++ src/main/java/spring_security/web/controller/HomeController.java
@@ -1,14 +1,30 @@
 package spring_security.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
 	// abrir pagina home
-	@GetMapping("/")
+	@GetMapping({"/", "/home"})
 	public String home() {
 		return "home";
-	}	
+	}
+
+	@GetMapping({"/login"})
+	public String login(){
+		return "login";
+	}
+
+	@GetMapping({"/login-error"})
+	public String loginError(ModelMap map){
+		map.addAttribute("alerta", "erro");
+		map.addAttribute("titulo", "Credenciais Inválidas!");
+		map.addAttribute("texto", "Login ou senha incorretos, tente novamente.");
+		map.addAttribute("subtexto", "Acesso permitido apenas para cadastros já ativados.");
+		return "login";
+	}
+
 }
